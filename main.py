2e86bf96926e13421e197fb6bc259eb3d57c211c
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-10-07 18:12:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from databases import Database
 import asyncio
 import asyncpg
+import logging
 #uvicorn main:app --reload
 
 
@@ -13,31 +14,26 @@ class Userdelete(BaseModel):
     name: str
 
 class Userupdate(BaseModel):
-    id: int
+    id: str
     name: str
 
 app = FastAPI()
 
+logging.basicConfig(filename='app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
 @app.post("/add")
 async def insert_records(parameters: Useradd):
     database = Database('postgresql://postgres:9785@localhost:5432/postgres')
     try:
         await database.connect()
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Connected to Database\n')
         import uuid
         uuid = uuid.uuid4()
         query = """INSERT INTO "user" (id,name) VALUES (:id ,:name)"""
         values = [{"id": f'{uuid}', "name": parameters.name}]
         await database.execute_many(query=query, values=values)
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write(f"""Inserted values in user Table\nid-{uuid}\nname-{parameters.name} Successfully\n""")
         await database.disconnect()
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Disconnecting from Database\n')
     except:
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Connection to Database Failed\n')
+          print('Connection to Database Failed')
 
 
 @app.delete("/delete")
@@ -45,19 +41,12 @@ async def user_delete(parameters: Userdelete):
     database = Database('postgresql://postgres:9785@localhost:5432/postgres')
     try:
         await database.connect()
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Connected to Database\n')
         query = """delete from "user" where name = :name"""
         values = [{"name": parameters.name}]
         await database.execute_many(query=query, values=values)
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write(f"""Delete values in user Table\nname-{parameters.name} Successfully\n""")
         await database.disconnect()
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Disconnecting from Database\n')
     except:
-        with open('log.txt', 'a', encoding='utf-8') as file:
-            file.write('Connection to Database Failed\n')
+            print('Connection to Database Failed\n')
 
 @app.post("/update")
 async def user_update(parameters: Userupdate):
@@ -65,7 +54,7 @@ async def user_update(parameters: Userupdate):
     try:
         await database.connect()
         print('Connected to Database')
-        query = """update "user" set name = :name where id = :id"""
+        query = """update "user" set name = :ame where id = :id"""
         values = [{"id": parameters.id, "name": parameters.name}]
         await database.execute_many(query=query, values=values)
         print('Update values in user Table Successfully')
